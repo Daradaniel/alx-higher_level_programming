e28aedffe48f138960d123771df3e4e3f5a394c9
Daradaniel <daviddaradaniel@gmail.com>
2022-12-20 17:42:51 +0100

    Like really

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,8 +2,10 @@
 
 """Empty class Square that defines a square
 
+python3 -c 'print(__import__("my_module").MyClass.__doc__)')
 Square: Class
 
 """
+
 class Square:
-    pass     
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Class Square defines a square
+
+python3 -c 'print(__import__("my_module").MyClass.__doc__)')
+Private instance attribute: size
+
+"""
+
+class Square:
+    def __init__(self, size):
+        self.__size = size
