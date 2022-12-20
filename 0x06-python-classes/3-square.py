437cc0b462ff307220ac7e898c48b83ea709b7ee
Daradaniel <daviddaradaniel@gmail.com>
2022-12-20 18:12:00 +0100

    Like really

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 
-"""class: Square
-
-python3 -c 'print(__import__("my_module").MyClass.__doc__)')
-Private instance attribute: size
+"""
 
 """
 
 class Square:
     def __init__(self, size=0):
-        if type(size) != int:
+        if size != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         
         self.__size = size
+
+    def area(self):
+        ar = self.__size * self.__size
+        return ar
