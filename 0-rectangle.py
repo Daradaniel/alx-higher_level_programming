5773ba0d269531bdd7bf3d55f392fc1ccb33d1e3
Daradaniel <daviddaradaniel@gmail.com>
2023-01-02 10:29:38 +0100

    Like really

---
+++ 0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module  has a class for rectangule instances.
+The module has the class Rectangle.
+"""
+
+class Rectangle:
+    """Class Rectangle
+    Class pass
+    """
+    def __init__(self):
+        pass
